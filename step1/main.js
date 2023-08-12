06c4994c9a8e84a0c5fa624aa8d8c1fa70147593
Yuka Suzuki <yukasuzuki@YukanoMacBook-Air.local>
2023-08-13 01:11:42 +0900

    開始と終了まで表示

--- step1/main.js
+++ step1/main.js
@@ -4,17 +4,42 @@ var startButton = document.getElementsByClassName('startButton')[0];
 var timer = null;
 startButton.addEventListener('click', function(){
     var seconds = 0;
-    timer = setInterval(function(){
-        seconds++;
-        console.log(seconds);
-        displayElm.innerHTML = seconds;
-    }, 1000);
+    // intervalIDをtimer変数に設定している
+    if(timer===null){
+        timer = setInterval(function(){
+            seconds++;
+            displayElm.innerHTML = seconds;
+            console.log(seconds);
+        }, 1000);
+    }
+    console.log('start:'+ timer);
+    var logElem = document.getElementsByClassName('log')[0];
+    var startText = document.createElement('p');
+    startText.innerText = '開始'
+    logElem.append(startText);
 });
 
 
 var stopButton = document.getElementsByClassName('stopButton')[0];
 stopButton.addEventListener('click', function(){
-    console.log(timer);
-    clearInterval(timer);
-    timer = null;
+    if(timer !== null) {
+        console.log('stop:'+ timer);
+        // 上の関数で設定したtimer変数(intervalID)をclearintervalで一時停止する
+        clearInterval(timer);
+        timer = null;
+        var logElem = document.getElementsByClassName('log')[0];
+        var endText = document.createElement('p');
+        endText.innerText = '終了'
+        logElem.append(endText);
+
+    }
 });
+
+
+
+
+// bugs
+// 01.  スタートボタンを複数回クリックしたらカウントの数字が２回表示される
+//      -- タイマーが動いている時にスタートをクリックしても何もしない
+// 02.　01の状態になるとEndボタンが機能しなくなる
+// 　　　-- タイマーが止まっている時にストップボタンをクリックしても何もしない
